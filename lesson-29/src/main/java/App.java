43f7557a81ae910cebf3337e1821e3b8cef853ae
Sergei <sleepytomcat@gmail.com>
2018-04-03 00:22:14 -0700

    Simple IoC bean loading

---
+++ lesson-29/src/main/java/App.java
@@ -0,0 +1,15 @@
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
+
+public class App {
+
+	public static void main(String[] args) {
+		// create application context
+		ApplicationContext ctx = new FileSystemXmlApplicationContext("beans.xml");
+		// instantiate bean
+		Organization org = (Organization) ctx.getBean("organization");
+		// call method
+		org.slogan();
+	}
+
+}
